f4e944ffd77558beb10f6d497b495f9c96032154
MOHAMED <ahmadiin612@gmail.com>
2025-01-04 22:53:12 +0300

    updated using props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,102 @@ export default function App() {
     return (
         <>
         <Header />
-        <Entry />
+        <Entry
+             image={{
+                  src: "https://scrimba.com/links/travel-journal-japan-image-url",
+                  alt: "mount fuji image"
+                  }}
+             markerimg="src/images/marker.png"
+             country="Japan"
+             link="https://www.google.com/maps/place/Mount+Fuji/"
+             title="Mount Fuji"
+             dates="Dates: 12 Jan, 2021 - 24 Jan, 2021"
+             description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
+        <Entry
+              image={{
+                  src: "",
+                  alt: ""
+                  }}
+              markerimg="src/images/marker.png"
+              country=""
+              link=""
+              title=""
+              dates=""
+              description=""
+        />
         </>
     )
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,17 +1,21 @@
-export default function  Entry() {
+export default function  Entry(props) {
+    const timeOfDay = new Date()
     return (
         <article className="journal-entry">
             <div className="main-image-container">
-            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji image" className="main-image"/>
+                <img src={props.image.src}  className="main-image" alt={props.image.alt}/>
             </div>
 
             <div className="container">
-                <img src="src/images/marker.png" alt="marker image" />
-                <span>Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google</a>
-                <h1>Mount Fuji</h1>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                 <div className="destination">
+                     <img src={props.markerimg} className="marker-image"/>
+                      <span>{props.country}</span>
+                      <a href={props.link}>View on Google</a>
+                  </div>
+
+                <h1>{props.title}</h1>
+                <p className="trip-dates">{props.dates}</p>
+                <p>{props.description}</p>
             </div>
         </article>
     )
