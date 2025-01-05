d02514c82b52088a0fa74ccab7926bede6bc66ea
MOHAMED <ahmadiin612@gmail.com>
2025-01-06 00:25:55 +0300

    fully developed travel journey

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,22 @@
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./data"
 
 export default function App() {
+    const  entryElement = data.map((entry) => {
+        return (
+            <Entry
+            key={entry.id}
+            {...entry}
+            />
+        )
+    })
     return (
         <>
         <Header />
-        <Entry
-             image={{
-                  src: "https://scrimba.com/links/travel-journal-japan-image-url",
-                  alt: "mount fuji image"
-                  }}
-             markerimg="src/images/marker.png"
-             country="Japan"
-             link="https://www.google.com/maps/place/Mount+Fuji/"
-             title="Mount Fuji"
-             dates="Dates: 12 Jan, 2021 - 24 Jan, 2021"
-             description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
-        <Entry
-              image={{
-                  src: "",
-                  alt: ""
-                  }}
-              markerimg="src/images/marker.png"
-              country=""
-              link=""
-              title=""
-              dates=""
-              description=""
-        />
+        <main className="container">
+             {entryElement}
+        </main>
         </>
     )
 }
